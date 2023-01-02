4244641f8b6623b0ff83b0061557cd95aba64094
Thomas Palmer <thomas@dualitystudio.co.uk>
2023-01-02 18:57:36 +0000

    Added vendor logos to tasks

--- resources/views/components/task.blade.php
+++ resources/views/components/task.blade.php
@@ -1,6 +1,7 @@
 <div class="divide-x divide-gray-200">
     @if ($task)
-        <div class="px-4 py-2 border-gray-200 text-md {{ $task->high_priority ? 'bg-red-300 text-red-800' : 'bg-gray-50  text-gray-800' }}">
+        <div
+            class="px-4 py-2 border-gray-200 text-md {{ $task->high_priority ? 'bg-red-300 text-red-800' : 'bg-gray-50  text-gray-800' }}">
             {{ $task->title }}
         </div>
 
@@ -14,21 +15,33 @@
             </article>
         </x-card-body>
 
-        <x-card-footer class="flex space-x-2">
-        <span class="inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
-             <i class="fa-solid fa-alarm-clock mr-1"></i>
-            <span>{{ $task->due_at->format("d/m/y") }}</span>
-        </span>
+        <x-card-footer class="flex justify-between items-center">
+            <div class="flex space-x-2">
+                <span class="inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
+                 <i class="fa-solid fa-alarm-clock mr-1"></i>
+                <span>{{ $task->due_at->format("d/m/y") }}</span>
+            </span>
 
-            <span class="inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium {{ $task->high_priority ? 'bg-red-300 text-red-800' : 'bg-gray-100 text-gray-800' }}">
-            <i class="fa-solid fa-light-emergency{{ $task->high_priority ? '-on' : '' }} mr-1"></i>
-            <span class="capitalize">{{ $task->priority }}</span>
-        </span>
+                <span
+                    class="inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium {{ $task->high_priority ? 'bg-red-300 text-red-800' : 'bg-gray-100 text-gray-800' }}">
+                <i class="fa-solid fa-light-emergency{{ $task->high_priority ? '-on' : '' }} mr-1"></i>
+                <span class="capitalize">{{ $task->priority }}</span>
+            </span>
 
-            <a href="{{ $task->link }}" class="inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
-                <i class="fa-solid fa-link mr-1"></i>
-                <span>Link</span>
-            </a>
+                <a href="{{ $task->link }}"
+                   class="inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
+                    <i class="fa-solid fa-link mr-1"></i>
+                    <span>Link</span>
+                </a>
+            </div>
+
+            <div>
+                @if($task->source?->vendor_info)
+                    <img src="{{ $task->source?->vendor_info['src'] }}" class="max-w-8 max-h-8" />
+                @else
+                    <x-logo />
+                @endif
+            </div>
         </x-card-footer>
     @else
         <x-card-body>
